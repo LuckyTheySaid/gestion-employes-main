5fa0c911d1190e98b7886602792b757e12db8425
LuckyTheySaid <taouafhero@gmail.com>
2023-01-26 13:56:03 +0000

    departement fixed

--- src/main/java/com/gestemployesDFM/Service/DepartementService.java
+++ src/main/java/com/gestemployesDFM/Service/DepartementService.java
@@ -1,6 +1,7 @@
 package com.gestemployesDFM.Service;
 
 import com.gestemployesDFM.entity.Departement;
+import com.gestemployesDFM.entity.Fonction;
 import com.gestemployesDFM.repository.DepartementRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,4 +17,13 @@ public class DepartementService {
 
         return drepo.findAll();
     }
+    public void save(Departement dpt) {
+
+        drepo.save(dpt);
+    }
+    public void delete(long id) {
+
+        drepo.deleteById(id);
+    }
+
 }
---
+++ src/main/java/com/gestemployesDFM/controller/DepartementController.java
@@ -0,0 +1,47 @@
+package com.gestemployesDFM.controller;
+
+import com.gestemployesDFM.Service.DepartementService;
+import com.gestemployesDFM.entity.Departement;
+import com.gestemployesDFM.entity.Fonction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.gestemployesDFM.entity.Departement;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class DepartementController {
+    @Autowired
+    private DepartementService depservice;
+
+    @GetMapping("/user/adddepartement")
+    public String ajouterdepartement(Model model){
+        model.addAttribute("departement", new Departement());
+        return "user-adddepartement";
+    }
+
+   /* @RequestMapping("/delete/{code}")
+    public String supprimerdepartement(@PathVariable(name = "code") int code, RedirectAttributes redirAttrs) {
+        redirAttrs.addFlashAttribute("supprimer", "Departement bien supprimer.");
+        depservice.delete(code);
+        return "redirect:/user-listdepartement";
+    }*/
+
+
+    @RequestMapping(value = "/ajouterdepartement", method = RequestMethod.POST)
+    public String savefct(@ModelAttribute("fonction") Departement dpt, RedirectAttributes redirAttrs) {
+        redirAttrs.addFlashAttribute("depajouter", "Departement bien ajouter.");
+        depservice.save(dpt);
+        return "redirect:/user/listdepartement";
+    }
+    @GetMapping("/user/listdepartement")
+    public String gestionfonction(Model model){
+
+        List<Departement> listdepartement = depservice.listAll();
+        model.addAttribute("listdepartement", listdepartement);
+        return "user-listdepartement";
+    }
+}
